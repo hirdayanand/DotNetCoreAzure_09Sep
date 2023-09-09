fa8c69717c6f830f618de9103d501c403504d4a5
hirdayanand <135501566+hirdayanand@users.noreply.github.com>
2023-09-09 14:36:03 +0530

    Initial Commit

---
+++ test/CoreWebAPIAzure.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+
+using CoreWebAPIAzure;
+using CoreWebAPIAzure.Controllers;
+using Xunit;
+namespace CoreWebAPIAzure.Test;
+
+public class UnitTest1
+{
+    ValuesController valuesController=new ValuesController();// Arrange
+    [Fact]
+    public void Test1()
+    {
+        var test=valuesController.Get(1); //Act
+        Assert.Equal("Yukti",test);
+
+    }
+}
